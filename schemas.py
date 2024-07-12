a6f0410bff505547f0fc30bb2f8c09431232fd0b
Tanim Khan <khantanim5640@gmail.com>
2024-07-12 01:20:01 -0700

    added jinja stuff

--- schemas.py
+++ schemas.py
@@ -4,7 +4,7 @@ from wtforms import StringField, validators
 from wtforms.validators import DataRequired
 
 class UserForm(FlaskForm):
-    username = StringField("username", [validators.Length(min=6, max=20, message="Username is a little too long there bud. 6-20 characters."), validators.InputRequired()], render_kw={"placeholder": "username"})
+    username = StringField("username", [validators.Length(min=6, max=20, message="Username is a little too long there bud. 6-20 characters."), validators.InputRequired()], render_kw={"autocorrect": "off", "autocapitalize": "off", "autocomplete": "off", "placeholder": "username"})
     email = StringField("email", [validators.InputRequired(), validators.Email()], render_kw={"autocorrect": "off", "autocapitalize": "off", "autocomplete": "off", "placeholder": "johndoe@gmail.com"})
-    password = StringField("password", [validators.Length(min=8, max=60, message="Password's gotta be between 8 and 60 characters long."), validators.InputRequired()], render_kw={"placeholder": "password"})
+    password = StringField("password", [validators.Length(min=8, max=60, message="Password's gotta be between 8 and 60 characters long."), validators.InputRequired()], render_kw={"autocorrect": "off", "autocapitalize": "off", "autocomplete": "off", "placeholder": "password"})
     
